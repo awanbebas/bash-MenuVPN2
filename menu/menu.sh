c171d914211daf9e952dab7e7f40f8c03031c131
awanbebas <135964469+awanbebas@users.noreply.github.com>
2023-06-13 16:33:46 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -322,7 +322,7 @@ case $opt in
 12) clear ; up ;;
 13) clear ; slow ;;
 99) clear ; clearcache ;;
-6969) clear ; wget https://raw.githubusercontent.com/awanbebas/vpn1/main/update.sh && chmod +x update.sh && ./update.sh && rm -f /root/update.sh ;;
+6969) clear ; wget https://raw.githubusercontent.com/awanbebas/vpn2/main/update.sh && chmod +x update.sh && ./update.sh && rm -f /root/update.sh ;;
 0) clear ; menu ;;
 x) exit ;;
 *) echo -e "" ; echo "Press any key to back exit" ; sleep 1 ; exit ;;
