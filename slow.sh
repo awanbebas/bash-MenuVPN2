10de313c67e6d08e5104ff3fe76460907c537bc9
awanbebas <135964469+awanbebas@users.noreply.github.com>
2023-06-13 23:18:21 +0700

    Update slow.sh

--- slow.sh
+++ slow.sh
@@ -3,7 +3,8 @@ dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Dat
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
 echo -e " [INFO] Sabar sayang Lagi Proses"
-wget https://raw.githubusercontent.com/Andyvpn/Autoscript-by-azi/main/autoscript-ssh-slowdns-main/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
+wget https://raw.githubusercontent.com/awanbebas/vpn2/main/autoscript-ssh-slowdns/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
 echo -e " [INFO] Selesai Ganti Ns nya Sayang , Enter"
 sleep 2
 menu
+
